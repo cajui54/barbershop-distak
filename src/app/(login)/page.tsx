12db0724c66ac6ad0c91b7faf43d3e1dba168334
cajui54 <cajui54@gmail.com>
2025-03-19 09:24:12 -0300

    � Commit inicial

---
+++ src/app/(login)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex h-full w-full items-center justify-center font-[family-name:var(--font-geist-sans)]">
+            <div className="h-[70%] w-[95%] overflow-hidden rounded-3xl bg-neutral-900 sm:w-[350px]"></div>
+        </div>
+    );
+}
