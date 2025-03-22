b0f46e21ad358c267363fb832984d8185a510824
cajui54 <cajui54@gmail.com>
2025-03-22 16:36:49 -0300

    feat: add component screen-info-user

--- src/_components/screen-info-user.tsx
+++ src/_components/screen-info-user.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
 const ScreenInfoUser = () => {
     return (
         <div className="mx-auto mt-4 flex h-[100px] w-[90%] items-center justify-between rounded-2xl bg-neutral-900 pl-2.5">
-            <div className="h-[90%] w-[60%] bg-red-500"></div>
+            <div className="flex h-[90%] w-[60%] items-center gap-x-2">
+                <Avatar className="flex h-[60px] w-[60px] items-center justify-center">
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>Image Profile</AvatarFallback>
+                </Avatar>
+
+                <div className="flex h-[98%] w-[75%] flex-col justify-center">
+                    <p className="text-sm">Seja bem vindo!</p>
+                    <p className="-mt-2 text-2xl font-bold text-emerald-500">
+                        Eduardo Santos
+                    </p>
+                    <p className="-mt-1 text-sm text-neutral-400 italic">
+                        edu@gmail.com
+                    </p>
+                </div>
+            </div>
             <div className="flex h-[90%] w-[30%] flex-col items-center justify-center">
-                <span>Março</span>
-                <span className="text-3xl text-emerald-400">21</span>
-                <span className="text-sm">2025</span>
+                <span className="capitalize">
+                    {format(new Date(), 'MMMM', {
+                        locale: ptBR,
+                    })}
+                </span>
+                <span className="-mt-2 text-3xl font-bold text-emerald-400">
+                    21
+                </span>
+                <span className="-mt-1 text-sm">2025</span>
             </div>
         </div>
     );
--- src/app/(login)/page.tsx
+++ src/app/(login)/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
 
     return (
         <div className="flex h-full w-full items-center justify-center font-[family-name:var(--font-geist-sans)]">
-            <div className="h-[400px] w-[85%] overflow-hidden rounded-3xl bg-neutral-900 sm:w-[300px]">
+            <div className="h-[500px] w-[70%] overflow-hidden rounded-3xl bg-neutral-900 sm:w-[400px]">
                 <div className="h-[220px] w-full overflow-hidden bg-neutral-900">
                     <div className="relative h-[250px] w-full overflow-hidden">
                         <div className="absolute bottom-0 z-30 h-full w-full bg-linear-to-b from-transparent to-neutral-900 to-80%"></div>
@@ -25,29 +25,38 @@ export default async function Home() {
                     </div>
                 </div>
                 <div className="mx-auto w-[80%]">
-                    <h2 className="text-2xl font-bold text-neutral-500">
-                        Destak Barbearia
+                    <h2 className="text-sm font-bold text-neutral-500">
+                        Seja Bem Vindo
                     </h2>
-                    <h3 className="-mt-2.5 ml-1.5 text-sm">
-                        Especializada em cortes
+                    <h3 className="-mt-2 text-3xl font-bold text-emerald-700">
+                        Barbearia Destak
                     </h3>
+                    <p className="-mt-2 ml-1.5 text-[12px]">
+                        Especializada em Cabelo - Barba - Pezinho
+                    </p>
                 </div>
-                <Button
-                    asChild
-                    className="mx-auto mt-[30px] flex w-[80%] items-center justify-center space-x-1 bg-emerald-700 text-white hover:bg-emerald-500"
-                >
-                    <Link href="/client">
-                        <FcGoogle />
-                        Entra com gmail do google
-                    </Link>
-                </Button>
 
-                <Link
-                    className="mt-8 block text-center text-sm tracking-wide text-neutral-500 underline"
-                    href="https://workspace.google.com/intl/pt-BR/gmail/"
-                >
-                    Não tem Gmail? Acesse aqui!
-                </Link>
+                <div className="mt-12">
+                    <p className="text-center text-[13px] text-neutral-500">
+                        Agendamentos de Horários e Serviços.
+                    </p>
+                    <Button
+                        asChild
+                        className="mx-auto mt-[5px] flex w-[80%] items-center justify-center space-x-1 bg-emerald-700 text-white hover:bg-emerald-500"
+                    >
+                        <Link href="/client">
+                            <FcGoogle />
+                            Entra com gmail do google
+                        </Link>
+                    </Button>
+
+                    <Link
+                        className="mt-8 block text-center text-sm tracking-wide text-neutral-500 underline"
+                        href="https://workspace.google.com/intl/pt-BR/gmail/"
+                    >
+                        Não tem Gmail? Acesse aqui!
+                    </Link>
+                </div>
             </div>
         </div>
     );
