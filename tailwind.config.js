df8dccbf99a78ef92cacc652317557032d9ac27b
cajui54 <cajui54@gmail.com>
2025-04-16 09:44:23 -0300

    chore: add file tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,html}',
+    './pages/**/*.{js,ts,jsx,tsx,html}',
+    './components/**/*.{js,ts,jsx,tsx,html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
