0e5667f0bb4a48e153fe2c6607365b2df042c178
cajui54 <cajui54@gmail.com>
2025-04-16 07:39:28 -0300

    fix: error of types

--- src/app/client/_components/button-service.tsx
+++ src/app/client/_components/button-service.tsx
@@ -4,22 +4,23 @@ import { Button } from '@/components/ui/button';
 import { ServiceItemProps } from '@/app/_components/service-item';
 import { useDispatch } from 'react-redux';
 import { addService, calculateTotal } from '@/_redux/serviceSlice';
+import { Prisma } from '@prisma/client';
 
 const ButtonService = ({ service }: ServiceItemProps) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleAddClient = () => {
-        dispatch(addService(service));
-        dispatch(calculateTotal());
-    };
-    return (
-        <Button
-            onClick={handleAddClient}
-            className="mt-1.5 ml-3.5 bg-emerald-700 text-sm text-white hover:bg-emerald-600"
-        >
-            Selecionar
-        </Button>
-    );
+  const handleAddClient = () => {
+    dispatch(addService({ ...service, price: Prisma.Decimal(service.price) }));
+    dispatch(calculateTotal());
+  };
+  return (
+    <Button
+      onClick={handleAddClient}
+      className="mt-1.5 ml-3.5 bg-emerald-700 text-sm text-white hover:bg-emerald-600"
+    >
+      Selecionar
+    </Button>
+  );
 };
 
 export default ButtonService;
